9f438007762bc2e659f3d98608888efb420f75b3
Micaela Jawor and Shawn Lunsford <micaela@>
2019-01-16 11:44:58 -0800

    make subtract, multiply, and divide work

--- js/scripts.js
+++ js/scripts.js
@@ -21,8 +21,29 @@ $(document).ready(function() {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2)
-    $("#results").text(result);
+    var addresult = add(number1, number2)
+    $("#addresults").text(addresult);
+  });
+  $("form#subtraction").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var subresult = subtract(number1, number2)
+    $("#subresults").text(subresult);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mul1").val());
+    var number2 = parseInt($("#mul2").val());
+    var mulresult = multiply(number1, number2)
+    $("#mulresults").text(mulresult);
+  });
+  $("form#division").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var divresult = divide(number1, number2)
+    $("#divresults").text(divresult);
   });
 });
 // $(document).ready(function() {
